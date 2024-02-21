7fc31454c591079448cb73052bd2619bea40c0b3
Anna Basenyuk <annabasenyuk@gmail.com>
2024-02-21 13:02:47 +0200

    add changes to index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/list_users_app">
+    <BrowserRouter basename="/task_planner_app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
